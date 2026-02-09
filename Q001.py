fa4d88aaa6e615290c2b5fedbad8c495d95e4e32
Armaan Sharma <armaan.official2306@gmail.com>
2026-02-09 12:45:05 +0530

    Add files via upload

---
+++ Q001.py
@@ -0,0 +1,4 @@
+# create a variable to store your age and print its type using type
+age = 18
+print (age)
+print (type (age))
---
+++ Q002.py
@@ -0,0 +1,3 @@
+# Declare a string variable called x and assign it the value "Hello, World!"
+x = "Hello, World!"
+print(x)
