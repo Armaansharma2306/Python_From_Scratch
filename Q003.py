23291c84e1f8aea62abc2471a67401e54bb4ab1d
Armaan Sharma <armaan.official2306@gmail.com>
2026-02-10 11:01:09 +0530

    Add files via upload

---
+++ Q003.py
@@ -0,0 +1,11 @@
+#Take different data types and print values using print fuction
+integer_number = 10
+float_number = 3.14
+string_value = "Hello, World!"
+boolean_value = True
+complex
+
+print(integer_number)
+print(float_number)
+print(string_value)
+print(boolean_value)
---
+++ Q004.py
@@ -0,0 +1,7 @@
+#  Declare these variables (x and y) as integers. Assign a value of 9 to x, Assign a value of 7 to y,perform addition, multiplication, division and subtraction on these two variables and Printout the result.
+x = 9
+y = 7
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
